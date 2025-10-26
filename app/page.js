f9b9b9fb1d9a1b0510628fc19942a4be62f726bf
Azeem Gulwani <azeemgulwani@usf.edu>
2025-10-26 02:06:01 -0400

    Integrated Counter component into Home page with default and custom prop instances

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
+import Counter from '../components/Counter';
+
 export default function Home() {
   return (
     <main style={{ textAlign: 'center', marginTop: '80px' }}>
       <h1>Next.js Button Counter</h1>
-      <p>
-        Reusable counter with increment, decrement, and reset functionality.
-      </p>
+      <p>Reusable counter with increment, decrement, and reset functionality.</p>
+
+      {/* Default Counter */}
+      <Counter />
+
+      {/* Counter with custom props */}
+      <Counter initialCount={5} initialStep={2} />
     </main>
   );
 }
